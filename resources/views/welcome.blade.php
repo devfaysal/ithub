6fc6c9a30cb91f7d2872826a859981fbacc920f6
Faysal Ahamed <miufaysal@live.com>
2020-08-17 22:27:34 +0600

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,9 +34,9 @@
                 </div>
                 <div class="hidden md:block md:ml-10 md:pr-4">
                     <a href="{{ route('home') }}" class="font-medium text-indigo-600 hover:text-indigo-900 transition duration-150 ease-in-out">Home</a>
-                    <a href="#" class="ml-8 font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">About</a>
-                    <a href="#" class="ml-8 font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">Services</a>
-                    <a href="#" class="ml-8 font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">Contact</a>
+                    <a href="#about" class="ml-8 font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">About</a>
+                    <a href="#services" class="ml-8 font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">Services</a>
+                    <a href="#contact" class="ml-8 font-medium text-gray-500 hover:text-gray-900 transition duration-150 ease-in-out">Contact</a>
                 </div>
                 </nav>
             </div>
@@ -76,9 +76,9 @@
                     </div>
                     <div class="px-2 pt-2 pb-3">
                     <a href="{{ route('home') }}" class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out" role="menuitem">Home</a>
-                    <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out" role="menuitem">About</a>
-                    <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out" role="menuitem">Services</a>
-                    <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out" role="menuitem">Contact</a>
+                    <a href="#about" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out" role="menuitem">About</a>
+                    <a href="services" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out" role="menuitem">Services</a>
+                    <a href="#contact" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50 focus:outline-none focus:text-gray-900 focus:bg-gray-50 transition duration-150 ease-in-out" role="menuitem">Contact</a>
                     </div>
                     <div>
                     {{-- <a href="#" class="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100 hover:text-indigo-700 focus:outline-none focus:bg-gray-100 focus:text-indigo-700 transition duration-150 ease-in-out" role="menuitem">
@@ -119,15 +119,15 @@
             <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="{{ asset('images/background.jpg') }}" alt="">
         </div>
     </div>
-    <div class="py-12 bg-white">
+    <div id="services" class="py-12 bg-gray-100">
         <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="lg:text-center">
-            <p class="text-base leading-6 text-indigo-600 font-semibold tracking-wide uppercase">Transactions</p>
+            <p class="text-base leading-6 text-indigo-600 font-semibold tracking-wide uppercase">Services</p>
             <h3 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
-                A better way to send money
+                With proven expertise
             </h3>
             <p class="mt-4 max-w-2xl text-xl leading-7 text-gray-500 lg:mx-auto">
-                Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in accusamus quisquam.
+                We craft websites, web applications and provide any kinds of IT support
             </p>
             </div>
 
@@ -143,9 +143,9 @@
                     </div>
                     </div>
                     <div class="ml-4">
-                    <h4 class="text-lg leading-6 font-medium text-gray-900">Competitive exchange rates</h4>
+                    <h4 class="text-lg leading-6 font-medium text-gray-900">Website Design</h4>
                     <p class="mt-2 text-base leading-6 text-gray-500">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                        We desgin business/corporate/company websites with modern technology
                     </p>
                     </div>
                 </div>
@@ -160,9 +160,9 @@
                     </div>
                     </div>
                     <div class="ml-4">
-                    <h4 class="text-lg leading-6 font-medium text-gray-900">No hidden fees</h4>
+                    <h4 class="text-lg leading-6 font-medium text-gray-900">E-Commerce Development</h4>
                     <p class="mt-2 text-base leading-6 text-gray-500">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                        We build exclusive E-Commerce applications with extensive features that attract customers
                     </p>
                     </div>
                 </div>
@@ -177,9 +177,9 @@
                     </div>
                     </div>
                     <div class="ml-4">
-                    <h4 class="text-lg leading-6 font-medium text-gray-900">Transfers are instant</h4>
+                    <h4 class="text-lg leading-6 font-medium text-gray-900">Web Application</h4>
                     <p class="mt-2 text-base leading-6 text-gray-500">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                        We craft Tailor-made Web Application that is highly scaleable
                     </p>
                     </div>
                 </div>
@@ -194,9 +194,61 @@
                     </div>
                     </div>
                     <div class="ml-4">
-                    <h4 class="text-lg leading-6 font-medium text-gray-900">Mobile notifications</h4>
+                    <h4 class="text-lg leading-6 font-medium text-gray-900">Mobile App</h4>
                     <p class="mt-2 text-base leading-6 text-gray-500">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                        We develop both native and hybrid mobile applications
+                    </p>
+                    </div>
+                </div>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </div>
+    <div id="contact" class="py-12 bg-white">
+        <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="lg:text-center">
+            <p class="text-base leading-6 text-indigo-600 font-semibold tracking-wide uppercase">Contact</p>
+            <h3 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+                Let's work together
+            </h3>
+            <p class="mt-4 max-w-2xl text-xl leading-7 text-gray-500 lg:mx-auto">
+                We would love to hear from you! Send us a message or an email
+            </p>
+            </div>
+
+            <div class="mt-10">
+            <ul class="md:grid md:grid-cols-2 md:col-gap-8 md:row-gap-10">
+                <li>
+                <div class="flex">
+                    <div class="flex-shrink-0">
+                    <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+                        </svg>
+                    </div>
+                    </div>
+                    <div class="ml-4">
+                    <h4 class="text-lg leading-6 font-medium text-gray-900">Email</h4>
+                    <p class="mt-2 text-base leading-6 text-gray-500">
+                        hello@faysal.me
+                    </p>
+                    </div>
+                </div>
+                </li>
+                <li class="mt-10 md:mt-0">
+                <div class="flex">
+                    <div class="flex-shrink-0">
+                    <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
+                        </svg>
+                    </div>
+                    </div>
+                    <div class="ml-4">
+                    <h4 class="text-lg leading-6 font-medium text-gray-900">Phone</h4>
+                    <p class="mt-2 text-base leading-6 text-gray-500">
+                        01670347708
                     </p>
                     </div>
                 </div>
